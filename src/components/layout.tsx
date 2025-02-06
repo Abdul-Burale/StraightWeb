480a7070660fabf3082a4f34fa5968a08ad5349a
Abdirahim  Burale <abdirahimburale@abdirahims-air.home>
2025-02-06 10:54:57 +0000

    Layout Complete

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,22 +1,21 @@
 export default function Layout() {
     return (
         <> 
-        <header className="min-h-15">
-            <h2 className="text-2xl font-semibold">Header</h2> 
-        </header>
-        <div className="flex h-screen overflow-hidden"> 
-            <aside className='w-[60] bg-black-100 lg:fle flex-col'>
-            <div className="flex h-14 items-center border-b px-4">
+        <div className="flex h-screen overflow-hidden border-t"> 
+            <aside className='w-56  lg:flex flex-col border-r'>
+            <div className="flex h-14 items-center px-20">
                 Sidebar
             </div>
-            
+            <div className="flex-1">
+            </div>
+            <div className="flex border-t h-50 px-20">
+                Account
+            </div>
             </aside>
-            <main className="flex-1 bg-gray-100 p-4">
-                Main
-            </main>
+            <div className="flex flex-1 flex-col overflow-hidden">
+                <header className="border-b h-14">StraightAPI</header>
+            </div>
         </div>
-
-        <footer className="bg-gray-800 text-white p-4">Footer</footer>
         </>
     );
 
